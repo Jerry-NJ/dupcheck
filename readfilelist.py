1bfebb66c47858511863f30607d8410c6ac279b5
Jerry.z <123229393@qq.com>
2014-03-21 16:52:02 +0800

    增加读取文件列表的测试

---
+++ readfilelist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*- 
+#set fileencodings=gb2312,utf-8
+#set fileencoding=gb2312
+#set termencoding=utf-8
+
+import sys
+import os
+import os.path
+reload(sys)
+sys.setdefaultencoding("utf8")
+
+listfile=os.listdir('/home/zyl')
+print listfile
